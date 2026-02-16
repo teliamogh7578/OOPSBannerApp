08fafa5e2c5eb4513217d9b8408d2d3d96b50c68
Amogh Teli <teliamogh7578@gmail.com>
2026-02-16 22:18:59 +0530

    UC2: Add ASCII banner display for OOPS

---
+++ OOPSBannerApp_UC2.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS to Console
+ * @author Amogh
+ * @version 2.0
+ */
+
+public class OOPSBannerApp_UC2 {
+
+    public static void main(String[] args) {
+        System.out.println( "***** " + " " + " ***** " + " " +" ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ****");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ** ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ****** " + " " +"******* ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "         **");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "      ** **");
+        System.out.println(" ***** " + " " + " ***** " + " " + " ** " + " " + "   ******* ");
+    }
+
+}
